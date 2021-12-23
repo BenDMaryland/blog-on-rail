fcdb59406117ccf4a904906aa124f9fe86ecbd60
My Name <ben.d.maryland@gmail.com>
2021-12-23 13:31:43 -0500

    portfolio v2

--- app/javascript/components/Portfolio.js
+++ app/javascript/components/Portfolio.js
@@ -6,24 +6,102 @@ import tick from "../assets/images/tick.png"
 
 
 
-function Portfolio({ scrollHander } ) {
+function Portfolio({ scrollHander }) {
 
 
 
 
     return (
+
+
         <Portfol id="portfolio" onMouseEnter={() => scrollHander("portfolio")}  >
             <div className="bar" > </div>
             <h1 id='projects_title' >Here Are Some Of My Projects</h1>
-
             <div className="project_container_left">
-                <div className="project" id="agile">
-                    <h2 className="title" > Agile solutions  </h2>
-                    <p className="descrip" > I solve Agile.  I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. I solve Agile. </p>
-                    <a className="link" > Click here </a>
+                <h2 className="title" > Agile solutions  </h2>
+                <div className='project_left'>
+                    <div className="project" id="agile">
+                        <h2>Description </h2>
+                        <p className="descrip" >A project management application where teams can create projects track progress create KBA's and see live feedback of team members performance. My final project for FlatIron. For this I wanted to create a project where I could track my own progress as I built the application.  </p>
+                       <h2>Difficulties  </h2>
+                        <p className="descrip" >My two biggest challenges for this project were, figuring out how to create the methods to track progress in the backend, and adding the teams functionality.  </p>
+                        <h2>Solutions </h2>
+                        <p className="descrip" >For serving data in the correct format I had to learn some new Ruby methods for managing time. Mainly, this allowed me to segregate data based on the dates which then allowed me to create the graphs to track progress. As for the teams functonality, that is a work in progress</p>
+                        <a className="links" href="https://bens-agile.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                        <a className="links" href="https://github.com/BenDMaryland/final-project-v4" target="_blank" rel="noopener noreferrer">github</a>
+                    </div>
+                    <div className='links_container'>
+                        <img className="profile_image_left" src={agile} />
+                        <h2>Features </h2>
+                        <ul>
+                            <li>The teams featurres allows multiple teams to work on independent projects separately  </li>
+                            <li>KBA's allow teams to have one location for as a repository for all data </li>
+                            <li>The Users and Projects tabs, allow managers to track User and project progress</li>
+                            <li>Admin and Boss role allows for separation of duties</li>
+                            <li>Email.js Allows for high Visability of high impact issues</li>
+                            <li>Draft.Js allows user to format KBA's using rich text </li>
+                        </ul>
+                    </div>
                 </div>
-                <img className="profile_image_left" src={agile} />
             </div>
+
+
+
+            <div className="project_container_right">
+
+                <h2 className="title" > The Goat Blog</h2>
+
+                <div className='project_right'>
+                                      <div className='links_container'>
+                        <img className="profile_image_left" src={blog} />
+
+
+
+                        <h2>Features </h2>
+                        <ul>
+                            <li>The teams featurres allows multiple teams to work on independent projects separately  </li>
+                            <li>KBA's allow teams to have one location for as a repository for all data </li>
+                            <li>The Users and Projects tabs, allow managers to track User and project progress</li>
+                            <li>Admin and Boss role allows for separation of duties</li>
+                            <li>Email.js Allows for high Visability of high impact issues</li>
+                            <li>Draft.Js allows user to format KBA's using rich text </li>
+                        </ul>
+                    </div>
+
+
+                    <div className="project" id="blog">
+
+                        <h2>Description </h2>
+                        <p className="descrip" >A project management application where teams can create projects track progress create KBA's and see live feedback of team members performance. My final project for FlatIron. For this I wanted to create a project where I could track my own progress as I built the application.  </p>
+
+
+                        <h2>Difficulties  </h2>
+                        <p className="descrip" >My two biggest challenges for this project were, figuring out how to create the methods to track progress in the backend, and adding the teams functionality.  </p>
+
+
+                        <h2>Solutions </h2>
+                        <p className="descrip" >For serving data in the correct format I had to learn some new Ruby methods for managing time. Mainly, this allowed me to segregate data based on the dates which then allowed me to create the graphs to track progress. As for the teams functonality, that is a work in progress</p>
+
+                        <a className="links" href="https://bens-agile.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Site</a>
+                        <a className="links" href="https://github.com/BenDMaryland/final-project-v4" target="_blank" rel="noopener noreferrer">github</a>
+
+                    </div>
+  
+
+
+
+
+
+
+                </div>
+
+            </div>
+
+
+
+
+
+
 
             <div className="project_container_right">
                 <img className="profile_image_right" src={blog} />
@@ -41,7 +119,7 @@ function Portfolio({ scrollHander } ) {
                     <p> A fully functional tick-tack-toe game, A fully functional tick-tack-toe game A fully functional tick-tack-toe game A fully functional tick-tack-toe game, A fully functional tick-tack-toe game A fully functional tick-tack-toe game </p>
                     <a> Click here </a>
                 </div>
-                <img className="profile_image_left" src={tick   } />
+                <img className="profile_image_left" src={tick} />
             </div>
 
             <div className="project_container_right">
@@ -62,7 +140,10 @@ const Portfol = styled.div`
 
 padding-bottom: 100px;
 background-color: #cb8cc2;
-
+h2{
+    color: #cb8cc2;
+      height: fit-content;
+}
 
 .bar{
     width: 100vw;
@@ -73,9 +154,9 @@ background-color: #cb8cc2;
 }
 
 .project_container_left{
-display: flex;
+display: grid;
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-padding-bottom: 40px;
+padding-bottom: 10px;
 padding-top: 40px;
 margin-left: 5%;
 margin-top: 15px;
@@ -83,14 +164,14 @@ background-color: #f5f5f5;
 }
 
 .profile_image_left{
-width: 30%;
+
 margin-left: 5%;
-height: auto;
   background-color: #f5f5f5;
 }
 
+
 .project_container_right{
-display: flex;
+display: grid;
 box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
 padding-bottom: 40px;
 background-color: #f5f5f5;
@@ -99,19 +180,19 @@ margin-right: 5%;
 margin-top: 15px;
 }
 .profile_image_right{
-width: 30%;
 margin-right: 5%;
-height: auto;
+height: 5%;
  background-color: #f5f5f5;
 }
+
 .project{
-    text-align: center;
+    /* text-align: center; */
    background-color: #f5f5f5;
-    display:grid;
+
    left: auto;
    right: auto;
-    width: 70%;
     height: auto;
+    padding:1%;
     p{
 width: 95%;
 color: black;
@@ -122,19 +203,13 @@ color: black;
 }
 .descrip{
   display: block;
+  height: fit-content;
 }
-a{
- width: auto;
-   margin: auto;
-}
+
 a:hover{
     background-color: #eaecec;
 }
 
-.link{
-
-  display: block;
-}
 #projects_title{
 text-align: center;
 font-family: 'Montserrat', sans-serif;
@@ -142,4 +217,24 @@ margin-top: 0ch;
 padding-top: 2%;
 
 }
+
+.project_left{
+display: grid;
+grid-template-columns:repeat(1, 2fr 1fr );
+height: fit-content;
+}
+.project_right{
+display: grid;
+grid-template-columns:repeat(1, 1fr 2fr );
+height: fit-content;
+}
+.links{
+
+ width: auto;
+   margin: auto;
+}
+img{
+    width:20vw;
+}
+
 `
